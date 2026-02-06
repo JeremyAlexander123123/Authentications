8bfd44512d6c77a04b20c7e4578b47ada35489de
JeremyAlexander <jeremy.daunan@gmail.com>
2026-02-06 10:10:13 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone"
+  module.exports = {
+    output: 'standalone',
+  }
 };
 
 export default nextConfig;
